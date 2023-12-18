68f9c4cca247ccd81aa2666cf0a3b031f3ff1f28
LUZ <feliluzz@proton.me>
2023-12-18 21:41:24 +0100

    fix: index.html

---
+++ timer.js
@@ -0,0 +1,15 @@
+<html>
+   <body>
+      <style>
+         .timer {
+           width:30vmin;
+     height:30vmin; 
+     border:1vh  solid green;
+     border-radius:50%;
+     background:conic-gradient(red 44%, 0, green )
+     }
+      </style>
+      <div class="timer">
+      </div>
+   </body>
+</html>
