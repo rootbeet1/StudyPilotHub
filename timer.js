407d56d514fa41b44439a5cfaa5a768cdc8843c2
LUZ <feliluzz@proton.me>
2023-12-18 22:42:43 +0100

    fix: circle timer

--- timer.js
+++ timer.js
@@ -1,15 +1,2 @@
-<html>
-   <body>
-      <style>
-         .timer {
-           width:30vmin;
-     height:30vmin; 
-     border:1vh  solid green;
-     border-radius:50%;
-     background:conic-gradient(red 44%, 0, green )
-     }
-      </style>
-      <div class="timer">
-      </div>
-   </body>
-</html>
+animation_id = window.requestAnimationFrame()
+window.cancelAnimationFrame(animation_id);
